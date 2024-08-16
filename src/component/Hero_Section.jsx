e1f8d582bdad79c6d2037aacedbf4a3444790db9
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-16 18:36:35 +0530

    add login btn for mobile view

--- src/component/Hero_Section.jsx
+++ src/component/Hero_Section.jsx
@@ -10,9 +10,13 @@ export default function Hero_Section() {
           <p className="lg:text-8xl md:text-6xl text-3xl z-50 text-white font-bold ">
             Start Something epic.
           </p>
-          <div className="flex justify-center mt-3 ">
-            <button className="p-3 px-5 md:text-md lg:text-lg font-bold text-white bg-pink-600 rounded-3xl border-[4px] border-white">
+          <div className="w-full text-center mt-3 ">
+            <button className="p-3 px-10 md:text-md lg:text-lg font-bold text-white bg-pink-600 rounded-3xl border-[4px] border-white">
               Crete Account
+            </button>
+            <br />
+            <button className="md:hidden mt-3 p-3 px-16 md:text-md lg:text-lg font-bold text-white bg-transparent rounded-3xl border-[4px] border-white">
+              Log In
             </button>
           </div>
         </div>
