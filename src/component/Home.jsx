9743b253b153e6b1ea1816cf5b8b45919c7a0851
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-16 18:50:36 +0530

    make bg fixed

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 function Home() {
   return (
     <div
-      className="w-full min-h-screen bg-cover "
+      className="w-full min-h-screen lg:bg-contain bg-fixed"
       style={{
         backgroundImage:
           "url('https://plugins-media.makeupar.com/smb/blog/post/2022-08-05/c414b386-9e1e-41c5-b87e-c930c41f34ed.jpg')",
