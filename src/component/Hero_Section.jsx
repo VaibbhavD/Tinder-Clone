c023aa712cb372836939f4407f6d3838cc347f07
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-16 18:50:54 +0530

    add play store btn on mobile view

--- src/component/Hero_Section.jsx
+++ src/component/Hero_Section.jsx
@@ -5,12 +5,12 @@ export default function Hero_Section() {
   return (
     <div className="bg-black bg-opacity-50">
       <Navbar />
-      <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <div className="min-h-screen min-w-screen flex justify-center items-center ">
         <div className="">
-          <p className="lg:text-8xl md:text-6xl text-3xl z-50 text-white font-bold ">
+          <p className="lg:text-8xl md:text-6xl text-4xl z-50 text-white font-bold ">
             Start Something epic.
           </p>
-          <div className="w-full text-center mt-3 ">
+          <div className="w-full text-center mt-3 pt-36 ">
             <button className="p-3 px-10 md:text-md lg:text-lg font-bold text-white bg-pink-600 rounded-3xl border-[4px] border-white">
               Crete Account
             </button>
@@ -18,6 +18,18 @@ export default function Hero_Section() {
             <button className="md:hidden mt-3 p-3 px-16 md:text-md lg:text-lg font-bold text-white bg-transparent rounded-3xl border-[4px] border-white">
               Log In
             </button>
+          </div>
+          <div class="md:hidden flex justify-center pt-10">
+            <div class="flex items-center border w-auto rounded-lg px-4 py-2 mx-2 bg-black text-white">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
+                class="w-7 md:w-8"
+              />
+              <div class="text-left ml-3">
+                <p class="text-xs text-gray-200">Download on </p>
+                <p class="text-sm md:text-base"> Google Play Store </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
