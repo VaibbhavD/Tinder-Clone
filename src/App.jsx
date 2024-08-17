7a234bcdbbe78553c56fc5b2fd3c545919221428
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-17 09:36:14 +0530

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { ContextProvider } from "./context/contextProvider";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./component/Home";
 import "./App.css";
-import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <ContextProvider>
       <BrowserRouter>
-        {/* <Navbar /> */}
         <Home />
       </BrowserRouter>
     </ContextProvider>
