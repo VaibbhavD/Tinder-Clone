bf5f8ab3a83ba4d3dd399017f337d17bee97507a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-17 09:13:48 +0530

    build modal for login comp pop up

---
+++ src/component/Modal.jsx
@@ -0,0 +1,45 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import React from "react";
+
+export default function Modal(props) {
+  return (
+    <>
+      <Transition appear show={props.isopen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10 bg-opacity-50 "
+          onClose={() => console.log("click model close ")}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center pt-20">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel>{props.children}</Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
