d36eeddb2f095a86f85f29dc64b3f2ce3174dae0
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-17 19:49:52 +0530

    define Routes and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { ContextProvider } from "./context/contextProvider";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./component/Home";
+import { ContextProvider } from "./context/ContextProvider";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/Home/Home";
+import UserForm from "./component/UserForm/UserForm";
+import { ToastContainer } from "react-toastify";
+import Dashboard from "./component/Dashboard/Dashboard";
+import { useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.authUser.isLoggedin);
+
   return (
     <ContextProvider>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {isLoggedIn && <Route path="/dashboard" element={<Dashboard />} />}
+        </Routes>
+        <ToastContainer />
+      </Router>
     </ContextProvider>
   );
 }
