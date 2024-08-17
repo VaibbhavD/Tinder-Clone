fe34cab7e31fa57a7b49f283ec7557a92ce7ee77
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-17 09:34:37 +0530

    add isLogin Popup state for onclick function on login

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -5,20 +5,29 @@ export const ContextProvider = (props) => {
   const localMode = localStorage.getItem("mode");
 
   const [Mode, SetMode] = useState(localMode);
+  const [isLoginPopup, SetisLoginPopup] = useState(false);
 
   const ToggleMode = () => {
     if (Mode === "white") {
       SetMode("dark");
       localStorage.setItem("mode", "dark");
+      document.body.style.backgroundColor = "White";
     } else {
       SetMode("white");
       localStorage.setItem("mode", "white");
+      document.body.style.backgroundColor = "rcb (17,24,39)";
     }
+  };
+
+  const LoginPopup = () => {
+    SetisLoginPopup((prev) => !prev);
   };
 
   const context = {
     mode: Mode,
     ToggleMode: ToggleMode,
+    isLoginPopup: isLoginPopup,
+    LoginPopup: LoginPopup,
   };
   return <Context.Provider value={context}>{props.children}</Context.Provider>;
 };
