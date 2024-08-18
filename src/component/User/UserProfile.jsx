daaf23b1b61d163b0327c8df2d9fbe78fc43f9c5
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-18 21:01:25 +0530

    add Users data into Userprofile

--- src/component/User/UserProfile.jsx
+++ src/component/User/UserProfile.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import Context from "../../context/context";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UserProfile() {
+  const context = useContext(Context);
+  const {
+    image,
+    birthDate,
+    firstName,
+    phoneNumber,
+    lastName,
+    gender,
+    relationship,
+    email,
+  } = context.User;
+
+  const navigate = useNavigate();
+
   return (
     <div class="">
       <div class="p-8 bg-white shadow mt-24">
@@ -28,55 +45,50 @@ function UserProfile() {
           <div class="relative">
             {" "}
             <div class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-24 w-24"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                {" "}
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
-                />
-              </svg>{" "}
+              <img src={image} />
             </div>{" "}
           </div>{" "}
           <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
-            <button class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+            <button
+              class="text-white py-2 px-4 uppercase rounded bg-orange-500 font-bold"
+              onClick={() => navigate("/onboard")}
+            >
               {" "}
-              Connect
-            </button>{" "}
-            <button class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-              {" "}
-              Message
+              Update Profile
             </button>{" "}
           </div>{" "}
         </div>{" "}
         <div class="mt-20 text-center border-b pb-12">
           {" "}
           <h1 class="text-4xl font-medium text-gray-700">
-            Jessica Jones, <span class="font-light text-gray-500">27</span>
+            {firstName} {lastName}
           </h1>{" "}
-          <p class="font-light text-gray-600 mt-3">Bucharest, Romania</p>{" "}
-          <p class="mt-8 text-gray-500">
-            Solution Manager - Creative Tim Officer
-          </p>{" "}
-          <p class="mt-2 text-gray-500">University of Computer Science</p>{" "}
         </div>{" "}
-        <div class="mt-12 flex flex-col justify-center">
-          {" "}
-          <p class="text-gray-600 text-center font-light lg:px-16">
-            An artist of considerable range, Ryan — the name taken by
-            Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
-            records all of his own music, giving it a warm, intimate feel with a
-            solid groove structure. An artist of considerable range.
-          </p>{" "}
-          <button class="text-indigo-500 py-2 px-4  font-medium mt-4">
-            {" "}
-            Show more
-          </button>{" "}
+        <div className="w-full md:w-full flex-grow p-4">
+          <div className="flex flex-wrap md:flex-nowrap mt-4 md:gap-4">
+            <div className="flex flex-col mb-4 w-full md-full">
+              <span className="font-bold">Email:</span>
+              <p>{email}</p>
+            </div>
+            <div className="flex flex-col mb-4 w-full md:w-1/2">
+              <span className="font-bold">Phone Number:</span>
+              <p>{phoneNumber}</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap md:flex-nowrap md:gap-4">
+            <div className="flex flex-col mb-4 w-full md:w-1/3">
+              <span className="font-bold">Birth Date:</span>
+              <p>{birthDate}</p>
+            </div>
+            <div className="flex flex-col mb-4 w-full md:w-1/3">
+              <span className="font-bold">Gender:</span>
+              <p>{gender}</p>
+            </div>
+            <div className="flex flex-col mb-4 w-full md:w-1/3">
+              <span className="font-bold">Relationship:</span>
+              <p>{relationship}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
