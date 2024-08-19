d465b7437b345c72a839ba2967d6dee6762acca0
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-19 10:28:27 +0530

    add some padding margin and update data

--- src/component/Testimonal/Testimonal.jsx
+++ src/component/Testimonal/Testimonal.jsx
@@ -8,9 +8,25 @@ function Testimonal() {
           class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
         >
           {" "}
-          <p className="text-left pb-2">Eujheb</p>
+          <p className="text-left pb-2">Rohit Patil</p>
           <hr />
-          <p className="leading-relaxed text-sm pt-2">
+          <p className="leading-relaxed text-sm pt-2 py-4">
+            “Switching to the new mobile phone from was the best decision I
+            made. The user interface is intuitive, and the performance is
+            lightning-fast. The transition was smooth, thanks to the excellent
+            support team who helped transfer all my data effortlessly.”
+          </p>
+        </div>
+      </div>
+
+      <div class="min-w-screen   ">
+        <div
+          class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
+        >
+          {" "}
+          <p className="text-left pb-2">Smitha Jain</p>
+          <hr />
+          <p className="leading-relaxed text-sm pt-2 py-4">
             “Switching to the new mobile phone from was the best decision I
             made. The user interface is intuitive, and the performance is
             lightning-fast. The transition was smooth, thanks to the excellent
@@ -24,9 +40,9 @@ function Testimonal() {
           class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
         >
           {" "}
-          <p className="text-left pb-2">Eujheb</p>
+          <p className="text-left pb-2">Aishwarya Marathe</p>
           <hr />
-          <p className="leading-relaxed text-sm pt-2">
+          <p className="leading-relaxed text-sm pt-2 py-4">
             “Switching to the new mobile phone from was the best decision I
             made. The user interface is intuitive, and the performance is
             lightning-fast. The transition was smooth, thanks to the excellent
@@ -40,9 +56,9 @@ function Testimonal() {
           class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
         >
           {" "}
-          <p className="text-left pb-2">Eujheb</p>
+          <p className="text-left pb-2">Sudhanshu Mishra</p>
           <hr />
-          <p className="leading-relaxed text-sm pt-2">
+          <p className="leading-relaxed text-sm pt-2 py-4">
             “Switching to the new mobile phone from was the best decision I
             made. The user interface is intuitive, and the performance is
             lightning-fast. The transition was smooth, thanks to the excellent
@@ -56,25 +72,9 @@ function Testimonal() {
           class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
         >
           {" "}
-          <p className="text-left pb-2">Eujheb</p>
+          <p className="text-left pb-2">Rajaram Patil</p>
           <hr />
-          <p className="leading-relaxed text-sm pt-2">
-            “Switching to the new mobile phone from was the best decision I
-            made. The user interface is intuitive, and the performance is
-            lightning-fast. The transition was smooth, thanks to the excellent
-            support team who helped transfer all my data effortlessly.”
-          </p>
-        </div>
-      </div>
-
-      <div class="min-w-screen  px-10 ">
-        <div
-          class={` p-5 text-center lg:w-96 w-60 text-white border-2 border-gray-400 rounded-xl hover:shadow-xl transition-shadow duration-300 ease-in-out `}
-        >
-          {" "}
-          <p className="text-left pb-2">Eujheb</p>
-          <hr />
-          <p className="leading-relaxed text-sm pt-2">
+          <p className="leading-relaxed text-sm pt-2 py-4">
             “Switching to the new mobile phone from was the best decision I
             made. The user interface is intuitive, and the performance is
             lightning-fast. The transition was smooth, thanks to the excellent
