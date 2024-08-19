65cbba9947f4d07c186a1fbef747bc2c9110316d
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-19 10:32:38 +0530

    Add PageLoader while get data from database

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -6,35 +6,42 @@ import Modal from "../Modal/Modal";
 import Login from "../Authentication/Login";
 import MobileSignup from "../Authentication/MobileSignup";
 import Context from "../../context/context";
+import PageLoader from "../Loader/PageLoader";
 
 function Home() {
   const context = useContext(Context);
   return (
-    <div
-      className="w-full min-h-screen lg:bg-contain bg-fixed relative"
-      style={{
-        backgroundImage:
-          "url('https://sketchelements.com/wp-content/uploads/2020/08/tinder-app-concept.png')",
-        // Add vintage effect
-      }}
-    >
-      {/* Pseudo-element for vignette effect */}
-      <div
-        className="absolute inset-0 bg-black opacity-30 pointer-events-none"
-        style={{
-          background: "radial-gradient(circle, transparent 60%, black 100%)",
-        }}
-      ></div>
-      <Hero_Section />
-      <Testimonal />
-      <Footer />
-      <Modal isshown={context.isLoginPopup}>
-        <Login />
-      </Modal>
-      <Modal isshown={context.isMobileLoginPopup}>
-        <MobileSignup />
-      </Modal>
-    </div>
+    <>
+      {!context.PageLoader && (
+        <div
+          className="w-full min-h-screen lg:bg-contain bg-fixed relative"
+          style={{
+            backgroundImage:
+              "url('https://sketchelements.com/wp-content/uploads/2020/08/tinder-app-concept.png')",
+            // Add vintage effect
+          }}
+        >
+          {/* Pseudo-element for vignette effect */}
+          <div
+            className="absolute inset-0 bg-black opacity-30 pointer-events-none"
+            style={{
+              background:
+                "radial-gradient(circle, transparent 60%, black 100%)",
+            }}
+          ></div>
+          <Hero_Section />
+          <Testimonal />
+          <Footer />
+          <Modal isshown={context.isLoginPopup}>
+            <Login />
+          </Modal>
+          <Modal isshown={context.isMobileLoginPopup}>
+            <MobileSignup />
+          </Modal>
+        </div>
+      )}
+      {context.PageLoader && <PageLoader />}
+    </>
   );
 }
 
