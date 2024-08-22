4e50da9a93334acceabe526b749cbf0dd906bd60
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-22 12:13:17 +0530

    Build Search Loader with user iamge

---
+++ src/component/Loader/SearchLoader.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import Context from "../../context/context";
+function SearchLoader() {
+  const context = useContext(Context);
+
+  return (
+    <div class="flex justify-center items-center h-screen">
+      <div class="relative flex justify-center items-center">
+        {/* <!-- Outer Expanding Ring 1 --> */}
+        <div class="absolute rounded-full h-12 w-12 border-4 border-violet-500 animate-expand"></div>
+        {/* <!-- Outer Expanding Ring 2 --> */}
+        <div class="absolute rounded-full h-12 w-12 border-4 border-violet-700 animate-expand-delayed"></div>
+        {/* <!-- Inner Ring --> */}
+        <div class="absolute rounded-full h-12 w-12 border-4 border-violet-900"></div>
+        {/* <!-- User Image --> */}
+        <img
+          src={context.User.image}
+          alt="User"
+          class="rounded-full h-16 w-16"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SearchLoader;
